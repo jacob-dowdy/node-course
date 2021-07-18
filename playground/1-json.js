9fcb175fb2c0b44ec3874ab1c814d1fc7d6afbe8
Jacob Dowdy <jacobdowdy@rocketmail.com>
2021-07-18 17:22:36 -0400

    add playground folder and example for how to pull and overwrite data

---
+++ playground/1-json.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+data.name = 'Jacob';
+data.age = 33;
+const newData = JSON.stringify(data);
+fs.writeFileSync('1-json.json', newData);
