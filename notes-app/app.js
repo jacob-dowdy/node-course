675f1ed92f19f6713ba7ef78e13ba6af9fe927db
Jacob Dowdy <jacobdowdy@rocketmail.com>
2021-09-29 16:15:46 -0400

    refactor methods to use arrow functions

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,64 +4,62 @@ const { removeNote } = require("./notes.js");
 const notes = require("./notes.js");
 
 // Customize yargs version
-yargs.version('1.1.0');
+yargs.version("1.1.0");
 
 // Create add command
 yargs.command({
-  command: 'add',
-  describe: 'Add a new note',
+  command: "add",
+  describe: "Add a new note",
   builder: {
     title: {
-      describe: 'Note title',
+      describe: "Note title",
       demandOption: true,
-      type: 'string'
+      type: "string"
     },
     body: {
-      describe: 'Note body',
+      describe: "Note body",
       demandOption: true,
-      type: 'string'
+      type: "string"
     }
   },
-  handler: function(argv) {
+  handler(argv) {
     notes.addNote(argv.title, argv.body);
   }
-})
+});
 
 // Create remove command
 yargs.command({
-  command: 'remove',
-  describe: 'Remove a note',
+  command: "remove",
+  describe: "Remove a note",
   builder: {
     title: {
-      describe: 'Note title',
+      describe: "Note title",
       demandOption: true,
-      type: 'string'
+      type: "string"
     }
   },
-  handler: function (argv) {
+  handler(argv) {
     notes.removeNote(argv.title);
   }
-})
+});
 
 // Create list command
 yargs.command({
-  command: 'list',
-  describe: 'List your notes',
-  handler: function () {
-    console.log('Listing out all notes');
+  command: "list",
+  describe: "List your notes",
+  handler() {
+    console.log("Listing out all notes");
   }
-})
+});
 
 // Create read command
 yargs.command({
-  command: 'read',
-  describe: 'Read a note',
-  handler: function () {
-    console.log('Reading a note');
+  command: "read",
+  describe: "Read a note",
+  handler() {
+    console.log("Reading a note");
   }
-})
-
-
+});
 
 yargs.parse();
 
