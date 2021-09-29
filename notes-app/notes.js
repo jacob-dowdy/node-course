a3449ee5228ef3ee8ccae3daef493e51b09e80fb
Jacob Dowdy <jacobdowdy@rocketmail.com>
2021-09-29 10:09:46 -0400

    add playground arrow function challenges

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,5 @@
-const { default: chalk } = require('chalk');
-const fs = require('fs');
+const { default: chalk } = require("chalk");
+const fs = require("fs");
 
 const getNotes = function () {
   return "Your notes...";
@@ -8,53 +8,53 @@ const getNotes = function () {
 const addNote = function (title, body) {
   const notes = loadNotes();
   const duplicateNotes = notes.filter(function (note) {
-    return note.title ===title;
-  })
+    return note.title === title;
+  });
 
   if (duplicateNotes.length === 0) {
     notes.push({
       title: title,
       body: body
-    })
-  
-    saveNotes(notes);
-    console.log("New note added!");
-  } else {
-    console.log("Note title taken!");
-  }
+    });
 
-}
+    saveNotes(notes);
+    console.log(chalk.green.inverse("New note added!"));
+  } else {
+    console.log(chalk.red.inverse("Note title taken!"));
+  }
+};
 
 const removeNote = function (title) {
   const notes = loadNotes();
   const notesToKeep = notes.filter(function (note) {
-    if (note.title === title) {
-      console.log(chalk.green('Note removed!'));
-    } else {
-      console.log(chalk.red('No note found!'));
-    }
-    return note.title !== title
-    })
+    return note.title !== title;
+  });
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse("Note removed!"));
     saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse("No note found!"));
   }
+};
 
 const saveNotes = function (notes) {
   const dataJSON = JSON.stringify(notes);
-  fs.writeFileSync('notes.json', dataJSON);
-}
+  fs.writeFileSync("notes.json", dataJSON);
+};
 
 const loadNotes = function () {
   try {
-    const dataBuffer = fs.readFileSync('notes.json');
+    const dataBuffer = fs.readFileSync("notes.json");
     const dataJSON = dataBuffer.toString();
     return JSON.parse(dataJSON);
   } catch (e) {
     return [];
   }
-}
+};
 
 module.exports = {
   getNotes: getNotes,
   addNote: addNote,
   removeNote: removeNote
-}
+};
